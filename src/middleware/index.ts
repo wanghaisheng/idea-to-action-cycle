854216fc1c0e81e591a069842c1538144f073a6a
wanghaisheng <edwin.uestc@gmail.com>
2023-07-12 13:35:54 +0800

    remove middleware

--- src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-import { i18nMiddleware } from "astro-i18n-aut"
-import { sequence } from "astro/middleware"
-
-const i18n = i18nMiddleware({ defaultLocale: "en" })
-
-export const onRequest = sequence(i18n)
