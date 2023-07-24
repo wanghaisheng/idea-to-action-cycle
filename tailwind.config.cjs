76b8cc1e5fe2118a88fc55933f009a47dbda428b
HeisenBerg? <edwin_uestc@163.com>
2023-07-24 15:25:20 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const plugin = require("tailwindcss/plugin")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
+	toolkit: {
 		extend: {
 			animation: {
 				float: "float 6s ease-in-out infinite",
